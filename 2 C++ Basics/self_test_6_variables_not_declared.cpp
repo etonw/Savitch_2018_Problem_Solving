8acdff353bc449576fca0e86548bf49c76fa776b
Eton Williams <13966710+etonw@users.noreply.github.com>
2022-06-13 10:56:36 +0200

    Create self_test_6_variables_not_declared.cpp
    /*
    
    self_test_6_variables_not_declared.cpp
    Savitch
    p.82
    
    6. Write a program that contains statements that output the value of five or
    six variables that have been declared, but not initialized. Compile and run
    the program. What is the output? Explain.
    
    */

---
+++ 2 C++ Basics/self_test_6_variables_not_declared.cpp
@@ -0,0 +1,27 @@
+/*
+
+self_test_6_variables_not_declared.cpp
+Savitch
+p.82
+
+6. Write a program that contains statements that output the value of five or
+six variables that have been declared, but not initialized. Compile and run
+the program. What is the output? Explain.
+
+*/
+
+
+#include <iostream>
+using namespace std;
+int main() {
+  
+int a, b, c, d, e, f;
+
+  
+  cout << " output  values " << endl << a ;
+  cout << b << endl;
+  cout << c << endl;
+  cout << d << endl;
+  cout << e << endl;
+  cout << f << endl;
+  }
